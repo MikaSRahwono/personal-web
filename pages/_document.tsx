798d395a8a71baa5943f8ee71aed31ad10b1b57c
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2025-08-25 15:47:42 +0700

    delete layout.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <meta name="description" content="One‑page portfolio with experience, projects, blogs, and social links." />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Mika — Personal Site</title>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
